0c7d9e551b916df0f71e52481711e2026b7773c0
Nikoloz Taturashvili <tnicko@proton.me>
2024-11-24 19:33:13 +0400

    add Program.cs

---
+++ TaskManager.Core/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace TaskManager.Core;
+
+internal static class Program
+{
+    public static void Main(string[] args)
+    {
+        // Possible use case at the moment
+        
+        /*var userDatabase = new UserDatabase();
+        var issueDatabase = new IssueDatabase();
+
+        var user1 = User.CreateUser("Nikoloz Taturashvili", "tnicko@proton.me", "12345678", userDatabase);
+        var user2 = User.CreateUser("Johnathan Markings", "johnmark@gmail.com", "12345678", userDatabase);
+        
+        
+        Issue.CreateIssue("TaskManager", Priority.High, issueDatabase, "Task Manager project", null, DateTime.Now.AddDays(7));
+        var issue2 = Issue.CreateIssue("Drink Water", Priority.Low, issueDatabase, "Hydration is important", user2);
+
+        foreach (var issue in issueDatabase.GetAllIssues())
+        {
+            issue.Print();
+            Console.WriteLine("\n");
+        }*/
+    }
+}
