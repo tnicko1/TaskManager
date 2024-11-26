e7ea7ca3c2d0729aa8f0aca67c83e84491a437da
Nikoloz Taturashvili <tnicko@proton.me>
2024-11-27 03:05:20 +0400

    update Program.cs to read from file

--- TaskManager.Core/Program.cs
+++ TaskManager.Core/Program.cs
@@ -6,20 +6,25 @@ internal static class Program
     {
         // Possible use case at the moment
         
-        /*var userDatabase = new UserDatabase();
+        var userDatabase = new UserDatabase();
         var issueDatabase = new IssueDatabase();
 
-        var user1 = User.CreateUser("Nikoloz Taturashvili", "tnicko@proton.me", "12345678", userDatabase);
-        var user2 = User.CreateUser("Johnathan Markings", "johnmark@gmail.com", "12345678", userDatabase);
-        
-        
-        Issue.CreateIssue("TaskManager", Priority.High, issueDatabase, "Task Manager project", null, DateTime.Now.AddDays(7));
-        var issue2 = Issue.CreateIssue("Drink Water", Priority.Low, issueDatabase, "Hydration is important", user2);
-
-        foreach (var issue in issueDatabase.GetAllIssues())
+        const string usersFile = "users.txt";
+        var allLines = File.ReadAllLines(usersFile);
+        foreach (var line in allLines)
         {
-            issue.Print();
-            Console.WriteLine("\n");
-        }*/
+            var parts = line.Split(',');
+            var name = parts[0];
+            var email = parts[1];
+            var password = parts[2];
+            var createdAt = DateTime.Parse(parts[3]);
+            User.CreateUser(name, email, password, userDatabase);
+        }
+
+        var users = userDatabase.GetAllUsers();
+        foreach (var user in users)
+        {
+            Console.WriteLine(user.GetName());
+        }
     }
 }
