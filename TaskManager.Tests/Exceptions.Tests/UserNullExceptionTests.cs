d898606a13e09a34faf6991502053fe00d15adf9
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:04:01 +0400

    add UserNullExceptionTests.cs

---
+++ TaskManager.Tests/Exceptions.Tests/UserNullExceptionTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using TaskManager.Core.Exceptions;
+
+namespace TaskManager.Tests.Exceptions.Tests;
+
+public class UserNullExceptionTests
+{
+    [Fact]
+    public void Exceptions_UserNullException_ShouldReturnMessage()
+    {
+        // Arrange
+        var exception = new UserNullException();
+        
+        // Act
+        var result = exception.Message;
+        
+        // Assert
+        result.Should().Be("User is null.");
+    }
+}
