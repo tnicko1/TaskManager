c3acb7cc94c251313794279bb9381b6064e59654
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:01:27 +0400

    add IUserDatabase.cs

---
+++ TaskManager.Core/Services/Abstractions/IUserDatabase.cs
@@ -0,0 +1,11 @@
+﻿namespace TaskManager.Core.Services.Abstractions;
+
+public interface IUserDatabase
+{
+    void AddUser(Models.User? user);
+    Models.User? GetUserById(Guid userId);
+    Models.User? GetUserByEmail(string email);
+    void UpdateUser(Models.User user);
+    void DeleteUser(Guid userId);
+    IEnumerable<Models.User?> GetAllUsers();
+}
