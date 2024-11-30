e7cd78a13ce56c9941a0e03cf6c51fd9ae3423ff
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 02:59:57 +0400

    add IIssueDatabase.cs

---
+++ TaskManager.Core/Services/Abstractions/IIssueDatabase.cs
@@ -0,0 +1,12 @@
+﻿using TaskManager.Core.Models;
+
+namespace TaskManager.Core.Services.Abstractions;
+
+public interface IIssueDatabase
+{
+    void AddIssue(Issue? issue);
+    Issue? GetIssueById(Guid issueId);
+    IEnumerable<Issue?> GetAllIssues();
+    void UpdateIssue(Issue issue);
+    void DeleteIssue(Guid issueId);
+}
