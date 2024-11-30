cbca74dee1c238624c1345f56531fa7bddc89964
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:01:18 +0400

    add IssueNullExceptionTests.cs

---
+++ TaskManager.Tests/Exceptions.Tests/IssueNullExceptionTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using TaskManager.Core.Exceptions;
+
+namespace TaskManager.Tests.Exceptions.Tests;
+
+public class IssueNullExceptionTests
+{
+    [Fact]
+    public void Exceptions_IssueNullException_ShouldReturnMessage()
+    {
+        // Arrange
+        var exception = new IssueNullException();
+        
+        // Act
+        var result = exception.Message;
+        
+        // Assert
+        result.Should().Be("Issue is null.");
+    }
+}
