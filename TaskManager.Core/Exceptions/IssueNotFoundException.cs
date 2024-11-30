c59255c503861e9a2319f026cfab99ce71efa00a
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:00:50 +0400

    add IssueNotFoundException.cs

---
+++ TaskManager.Core/Exceptions/IssueNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Core.Exceptions;
+
+public class IssueNotFoundException : Exception
+{
+    public IssueNotFoundException() : base("Issue not found.") { }
+    
+    public IssueNotFoundException(string message) : base(message) { }
+    
+    public IssueNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+}
