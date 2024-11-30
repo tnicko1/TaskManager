4f0d55b2a9ce20a02a7bc9cf81a4718822e4223e
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:00:06 +0400

    add InvalidStatusChangeException.cs

---
+++ TaskManager.Core/Exceptions/InvalidStatusChangeException.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Core.Exceptions;
+
+public class InvalidStatusChangeException : Exception
+{
+    public InvalidStatusChangeException() : base("Invalid status change.") { }
+    
+    public InvalidStatusChangeException(string message) : base(message) { }
+    
+    public InvalidStatusChangeException(string message, Exception innerException) : base(message, innerException) { }
+}
