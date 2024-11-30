16e8ad1e64158b33c87c5a7421994e92b8224f0b
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:03:39 +0400

    add UserNotFoundException.cs

---
+++ TaskManager.Core/Exceptions/UserNotFoundException.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Core.Exceptions;
+
+public class UserNotFoundException : Exception
+{
+    public UserNotFoundException() : base("User not found") { }
+
+    public UserNotFoundException(string message) : base(message) { }
+
+    public UserNotFoundException(string message, Exception innerException) : base(message, innerException) { }
+}
