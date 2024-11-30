44bf40292f6e1c5e09c6c149a4b617347179229a
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:03:54 +0400

    add UserNullException.cs

---
+++ TaskManager.Core/Exceptions/UserNullException.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Core.Exceptions;
+
+public class UserNullException : Exception
+{
+    public UserNullException() : base("User is null.") { }
+    
+    public UserNullException(string message) : base(message) { }
+    
+    public UserNullException(string message, Exception innerException) : base(message, innerException) { }
+}
