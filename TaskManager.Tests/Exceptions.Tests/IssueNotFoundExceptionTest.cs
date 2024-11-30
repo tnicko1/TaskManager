ee1cfc4813a330c400a8e46abb53f5bcf1bc9add
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:00:59 +0400

    add IssueNotFoundExceptionTest.cs

---
+++ TaskManager.Tests/Exceptions.Tests/IssueNotFoundExceptionTest.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using TaskManager.Core.Exceptions;
+
+namespace TaskManager.Tests.Exceptions.Tests;
+
+public class IssueNotFoundExceptionTest
+{
+    [Fact]
+    public void Exceptions_IssueNotFoundException_ShouldReturnMessage()
+    {
+        // Arrange
+        var exception = new IssueNotFoundException();
+        
+        // Act
+        var result = exception.Message;
+        
+        // Assert
+        result.Should().Be("Issue not found.");
+    }
+}
