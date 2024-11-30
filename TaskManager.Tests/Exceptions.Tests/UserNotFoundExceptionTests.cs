ff06605a3b41cb9a30bd57e1bb94c8f0c6ad5d4c
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:03:46 +0400

    add UserNotFoundExceptionTests.cs

---
+++ TaskManager.Tests/Exceptions.Tests/UserNotFoundExceptionTests.cs
@@ -0,0 +1,19 @@
+﻿using FluentAssertions;
+using TaskManager.Core.Exceptions;
+
+namespace TaskManager.Tests.Exceptions.Tests;
+
+public class UserNotFoundExceptionTests
+{
+    [Fact]
+    public void Exceptions_UserNotFoundException_ShouldReturnMessage() {
+        // Arrange
+        var exception = new UserNotFoundException();
+        
+        // Act
+        var result = exception.Message;
+        
+        // Assert
+        result.Should().Be("User not found");
+    }
+}
