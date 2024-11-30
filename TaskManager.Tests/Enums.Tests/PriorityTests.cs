7f94e8cefc8b8b15e5a4215d785fa62fdab0b68f
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:01:45 +0400

    add PriorityTests.cs

---
+++ TaskManager.Tests/Enums.Tests/PriorityTests.cs
@@ -0,0 +1,46 @@
+﻿using TaskManager.Core.Enums;
+using FluentAssertions;
+
+namespace TaskManager.Tests.Enums.Tests;
+
+public class PriorityTests
+{
+    [Fact]
+    public void Priority_High_ShouldReturnHigh()
+    {
+        // Arrange
+        const Priority priority = Priority.High;
+
+        // Act
+        var result = priority.ToString();
+
+        // Assert
+        priority.Should().Be(Priority.High);
+    }
+    
+    [Fact]
+    public void Priority_Medium_ShouldReturnMedium()
+    {
+        // Arrange
+        const Priority priority = Priority.Medium;
+
+        // Act
+        var result = priority.ToString();
+
+        // Assert
+        priority.Should().Be(Priority.Medium);
+    }
+
+    [Fact]
+    public void Priority_Low_ShouldReturnLow()
+    {
+        // Arrange
+        const Priority priority = Priority.Low;
+
+        // Act
+        var result = priority.ToString();
+
+        // Assert
+        priority.Should().Be(Priority.Low);
+    }
+}
