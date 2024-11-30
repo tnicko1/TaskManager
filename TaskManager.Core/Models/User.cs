fbf1f981e055fb9e34e9cb2757017b5b21630f6a
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:03:23 +0400

    update User.cs

---
+++ TaskManager.Core/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+using TaskManager.Core.Services.Implementations;
+
+namespace TaskManager.Core.Models;
+
+public partial class User
+{
+    public required string? Name { get; set; }
+    public required string? Email { get; set; }
+    public required string? Password { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public Guid Id { get; set; }
+    public static UserDatabase? Database { get; set; }
+    
+    public User()
+    {
+        Database?.AddUser(this);
+    }
+}
