67ed920126701068bd4fa4cd7bd47425d7dd1ea9
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:01:08 +0400

    add IssueNullException.cs

---
+++ TaskManager.Core/Exceptions/IssueNullException.cs
@@ -0,0 +1,10 @@
+﻿namespace TaskManager.Core.Exceptions;
+
+public class IssueNullException : Exception
+{
+    public IssueNullException() : base("Issue is null.") { }
+    
+    public IssueNullException(string message) : base(message) { }
+    
+    public IssueNullException(string message, Exception innerException) : base(message, innerException) { }
+}
