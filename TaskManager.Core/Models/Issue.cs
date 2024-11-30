c2651c4c011930fe18de09bd49bcc8144af6aeee
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:00:29 +0400

    update Issue.cs

---
+++ TaskManager.Core/Models/Issue.cs
@@ -0,0 +1,18 @@
+﻿using TaskManager.Core.Enums;
+using TaskManager.Core.Services.Implementations;
+
+namespace TaskManager.Core.Models;
+
+public class Issue {
+    public required string Title { get; set; }
+    public string? Description { get; set; }
+    public User? Assignee { get; set; }
+    public required Priority Priority { get; set; }
+    public DateTime? CreatedAt { get; set; }
+    public DateTime? Due { get; set; }
+    public DateTime? FinishedAt { get; set; }
+    public Status Status { get; set; }
+    public Guid Id{ get; set; }
+    
+    public static IssueDatabase? Database { get; set; }
+}
