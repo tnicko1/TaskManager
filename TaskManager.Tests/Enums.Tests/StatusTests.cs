b1f3867be6f22eeb4e6534df424150027c0e1f3a
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:02:11 +0400

    add StatusTests.cs

---
+++ TaskManager.Tests/Enums.Tests/StatusTests.cs
@@ -0,0 +1,46 @@
+﻿using TaskManager.Core.Enums;
+using FluentAssertions;
+
+namespace TaskManager.Tests.Enums.Tests;
+
+public class StatusTests
+{
+    [Fact]
+    public void Status_ToDo_ShouldReturnToDo()
+    {
+        // Arrange
+        const Status status = Status.Todo;
+
+        // Act
+        var result = status.ToString();
+
+        // Assert
+        status.Should().Be(Status.Todo);
+    }
+    
+    [Fact]
+    public void Status_InProgress_ShouldReturnInProgress()
+    {
+        // Arrange
+        const Status status = Status.InProgress;
+
+        // Act
+        var result = status.ToString();
+
+        // Assert
+        status.Should().Be(Status.InProgress);
+    }
+
+    [Fact]
+    public void Status_Done_ShouldReturnDone()
+    {
+        // Arrange
+        const Status status = Status.Done;
+
+        // Act
+        var result = status.ToString();
+
+        // Assert
+        status.Should().Be(Status.Done);
+    }
+}
