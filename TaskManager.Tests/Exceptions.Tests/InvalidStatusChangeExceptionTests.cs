499acd2c52b4af8d2aad2150c1e663812c3731a9
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 03:00:12 +0400

    add InvalidStatusChangeExceptionTests.cs

---
+++ TaskManager.Tests/Exceptions.Tests/InvalidStatusChangeExceptionTests.cs
@@ -0,0 +1,20 @@
+﻿using FluentAssertions;
+using TaskManager.Core.Exceptions;
+
+namespace TaskManager.Tests.Exceptions.Tests;
+
+public class InvalidStatusChangeExceptionTests
+{
+    [Fact]
+    public void Exceptions_InvalidStatusChangeException_ShouldReturnMessage()
+    {
+        // Arrange
+        var exception = new InvalidStatusChangeException();
+        
+        // Act
+        var result = exception.Message;
+        
+        // Assert
+        result.Should().Be("Invalid status change.");
+    }
+}
