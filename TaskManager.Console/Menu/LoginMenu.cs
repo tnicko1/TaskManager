4e69ab25227f11dcf90f57089a6051f39c39fa37
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 06:04:47 +0400

    add LoginMenu.cs

---
+++ TaskManager.Console/Menu/LoginMenu.cs
@@ -0,0 +1,39 @@
+﻿using TaskManager.Core.Models;
+
+namespace TaskManager.Console.Menu;
+
+public static class LoginMenu
+{
+    public static void Show()
+    {
+        System.Console.WriteLine("Please login to continue.");
+        System.Console.WriteLine("Email: ");
+        var email = System.Console.ReadLine();
+        System.Console.WriteLine("Password: ");
+        var password = System.Console.ReadLine();
+
+        if (email == null) return;
+        var user = User.Database?.GetUserByEmail(email);
+        if (user == null)
+        {
+            System.Console.ForegroundColor = System.ConsoleColor.Red;
+            System.Console.WriteLine("User not found.");
+            System.Console.ResetColor();
+            return;
+        }
+
+        if (user.Password != password)
+        {
+            System.Console.ForegroundColor = System.ConsoleColor.Red;
+            System.Console.WriteLine("Invalid password.");
+            System.Console.ResetColor();
+            return;
+        }
+
+        System.Console.ForegroundColor = System.ConsoleColor.Green;
+        System.Console.WriteLine($"Hello, {user.Name}!");
+        System.Console.ResetColor();
+        
+        Dashboard.HomeScreen.Show();
+    }
+}
