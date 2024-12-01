e172ce750d9d160551793a07024ad1204e2a6fe2
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 06:04:40 +0400

    add LoadUsers.cs

---
+++ TaskManager.Console/LoadMethods/LoadUsers.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+using TaskManager.Core.Models;
+using TaskManager.Core.Services.Implementations;
+
+namespace TaskManager.Console.LoadMethods;
+
+public static class LoadUsers
+{
+    public static void LoadUsersFromJson()
+    {
+        User.Database = new UserDatabase();
+        
+        var usersJson = File.ReadAllText(Path.Combine("..", "..", "..", "Jsons", "users.json"));
+        if (string.IsNullOrWhiteSpace(usersJson)) return;
+        var users = JsonSerializer.Deserialize<List<User>>(usersJson);
+        if (users == null) return;
+        foreach (var user in users)
+        {
+            if (user.Email != null && User.Database?.GetUserByEmail(user.Email) == null)
+            {
+                User.Database?.AddUser(user);
+            }
+        }
+    }
+}
