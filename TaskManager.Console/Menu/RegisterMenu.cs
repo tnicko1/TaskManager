41ff029b7f22a8f808daaf603f267ee28725bd2e
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 06:05:08 +0400

    add RegisterMenu.cs

---
+++ TaskManager.Console/Menu/RegisterMenu.cs
@@ -0,0 +1,35 @@
+﻿using TaskManager.Core.Commands;
+
+namespace TaskManager.Console.Menu;
+
+public static class RegisterMenu
+{
+    public static void Show()
+    {
+        System.Console.WriteLine("Please register to continue.");
+        System.Console.WriteLine("Name: ");
+        var name = System.Console.ReadLine();
+        System.Console.WriteLine("Email: ");
+        var email = System.Console.ReadLine();
+        System.Console.WriteLine("Password: ");
+        var password = System.Console.ReadLine();
+
+        if (name == null || email == null || password == null) return;
+        var createUserCommand = new CreateUserCommand(name, email, password);
+
+        try
+        {
+            createUserCommand.Execute();
+        } catch (System.ArgumentException e)
+        {
+            System.Console.ForegroundColor = System.ConsoleColor.Red;
+            System.Console.WriteLine(e.Message);
+            System.Console.ResetColor();
+            return;
+        }
+
+        System.Console.ForegroundColor = System.ConsoleColor.Green;
+        System.Console.WriteLine("User created successfully.");
+        System.Console.ResetColor();
+    }
+}
