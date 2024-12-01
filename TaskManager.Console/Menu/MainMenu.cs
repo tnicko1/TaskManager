4902ed74996a69ffa5e0c3eb4c902d5853d767cd
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 06:04:53 +0400

    add MainMenu.cs

---
+++ TaskManager.Console/Menu/MainMenu.cs
@@ -0,0 +1,32 @@
+﻿namespace TaskManager.Console.Menu;
+
+public class MainMenu
+{
+    public static void Show()
+    {
+        while (true)
+        {
+            System.Console.WriteLine("Welcome to Task Manager!");
+            System.Console.WriteLine("1. Login");
+            System.Console.WriteLine("2. Register");
+            System.Console.WriteLine("3. Exit");
+            var choice = Convert.ToInt32(System.Console.ReadLine());
+            switch (choice)
+            {
+                case 1:
+                    LoginMenu.Show();
+                    break;
+                case 2:
+                    RegisterMenu.Show();
+                    break;
+                case 3:
+                    return;
+                default:
+                    System.Console.ForegroundColor = System.ConsoleColor.Red;
+                    System.Console.WriteLine("Invalid choice.");
+                    System.Console.ResetColor();
+                    break;
+            }
+        }
+    }
+}
