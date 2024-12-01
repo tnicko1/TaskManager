051dbac40d63c42d1343a03419359c5afe8315a9
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 06:05:32 +0400

    add SavedIssues.cs

---
+++ TaskManager.Console/SaveMethods/SaveIssues.cs
@@ -0,0 +1,21 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using TaskManager.Core.Models;
+
+namespace TaskManager.Console.SaveMethods;
+
+public static class SaveIssues
+{
+    public static void SaveIssuesToJson()
+    {
+        var issues = Issue.Database?.GetAllIssues();
+        if (issues == null) return;
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Converters = { new JsonStringEnumConverter() }
+        };
+        var issuesJson = JsonSerializer.Serialize(issues, options);
+        File.WriteAllText(Path.Combine("..", "..", "..", "Jsons", "issues.json"), issuesJson);
+    }
+}
