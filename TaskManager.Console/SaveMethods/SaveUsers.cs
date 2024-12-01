9ec51be8ae4441ac1cef0e366e0c298639127263
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 06:05:43 +0400

    add SaveUsers.cs

---
+++ TaskManager.Console/SaveMethods/SaveUsers.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using TaskManager.Core.Models;
+
+namespace TaskManager.Console.SaveMethods;
+
+public static class SaveUsers
+{
+    public static void SaveUsersToJson()
+    {
+        var users = User.Database?.GetAllUsers();
+        if (users == null) return;
+        var options = new JsonSerializerOptions
+        {
+            WriteIndented = true,
+        };
+        var usersJson = JsonSerializer.Serialize(users, options);
+        File.WriteAllText(Path.Combine("..", "..", "..", "Jsons", "users.json"), usersJson);
+    }
+}
