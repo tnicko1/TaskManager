81427c4aae8ac8d29031b7a60eb6624ec3c8ade1
Nikoloz Taturashvili <tnicko@proton.me>
2024-12-01 06:03:46 +0400

    add DeleteIssueCommand.cs

---
+++ TaskManager.Core/Commands/DeleteIssueCommand.cs
@@ -0,0 +1,29 @@
+﻿using TaskManager.Core.Models;
+
+namespace TaskManager.Core.Commands;
+
+public class DeleteIssueCommand(Guid id)
+{
+    private Guid Id { get; set; } = id;
+    public void Execute()
+    {
+        Validate();
+        {
+            Issue.Database?.DeleteIssue(Id);
+        }
+    }
+    
+    private void Validate()
+    {
+        if (Issue.Database == null)
+        {
+            throw new InvalidOperationException("Issue database is not initialized.");
+        }
+
+        var issue = Issue.Database.GetIssueById(Id);
+        if (issue == null)
+        {
+            throw new ArgumentException("Issue not found.");
+        }
+    }
+}
